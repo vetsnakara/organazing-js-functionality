7d61274822ab668022fc111a003b4685649ea53a
Konstantin Arakantsev <kostya3312@gmail.com>
2020-11-09 21:07:10 +0300

    make details work

--- js/details.js
+++ js/details.js
@@ -1,15 +1,16 @@
-$(document).ready(function(){
+$(function () {
+  var $carousel = $("[rel=js-carousel] > [rel=js-content]");
+  var $content = $("[rel=js-details]");
 
-	var $items = $("[rel=js-carousel] > [rel=js-content] > [rel=js-items]");
-	var $content = $("[rel=js-details]");
+  $carousel.on("click", '[rel^="js-"]', function (event) {
+    var $item = $(event.target);
+    var rel = $item.attr("rel");
+    var id = rel.replace(/^.*(\d+)$/, "$1");
 
-	// on click of a carousel item, do an Ajax request for
-	// the "details/2.html" (or whatever) file for the person
-	// clicked, and load those contents into the `$content` div.
-
-	// hint: you will probably want to use "event propagation"
-	// (aka "event delegation"), by attaching a single event
-	// handler the `$content` element rather than individual
-	// event handlers to each item in the carousel.
-
+    $.ajax("details/" + id + ".html", { dataType: "text" }).then(function (
+      content
+    ) {
+      $content.html(content);
+    });
+  });
 });
