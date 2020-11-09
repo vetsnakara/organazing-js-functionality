80b5b84c4c837f3c3fc557b2daf56322b4eada8b
Konstantin Arakantsev <kostya3312@gmail.com>
2020-11-09 18:01:58 +0300

    make carousel work

--- js/carousel.js
+++ js/carousel.js
@@ -1,42 +1,38 @@
-$(document).ready(function(){
+$(function () {
+  function scrollLeft(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    evt.stopImmediatePropagation();
 
-	function scrollLeft(evt) {
-		evt.preventDefault();
-		evt.stopPropagation();
-		evt.stopImmediatePropagation();
+    if (position > 0) {
+      position = Math.max(0, position - 250);
+    }
 
-		if (position > 0) {
-			position = Math.max(0,position - 250);
-		}
+    $items.css({ left: -position + "px" });
+  }
 
-		$items.css({ left: (-position) + "px" });
-	}
+  function scrollRight(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    evt.stopImmediatePropagation();
 
-	function scrollRight(evt){
-		evt.preventDefault();
-		evt.stopPropagation();
-		evt.stopImmediatePropagation();
+    if (position < maxPosition) {
+      position = Math.min(maxPosition, position + 250);
+    }
 
-		if (position < maxPosition) {
-			position = Math.min(maxPosition,position + 250);
-		}
+    $items.css({ left: -position + "px" });
+  }
 
-		$items.css({ left: (-position) + "px" });
-	}
+  var $content = $("[rel=js-carousel] > [rel=js-content]");
+  var $items = $content.children("[rel=js-items]");
+  var $left = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-left]");
+  var $right = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-right]");
 
-	var $content = $("[rel=js-carousel] > [rel=js-content]");
-	var $items = $content.children("[rel=js-items]");
-	var $left = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-left]");
-	var $right = $("[rel=js-carousel] > [rel=js-controls] > [rel=js-right]");
+  var contentWidth = $content.width();
+  var itemsWidth = $items.width();
+  var position = 0;
+  var maxPosition = itemsWidth - contentWidth;
 
-
-	var contentWidth = $content.width();
-	var itemsWidth = $items.width();
-	var position = 0;
-	var maxPosition = (itemsWidth - contentWidth);
-
-	// attach click handlers for the `$left` and `$right` buttons,
-	// that call the `scrollLeft(..)` and `scrollRight(..)` functions,
-	// respectively
-
+  $left.on("click", scrollLeft);
+  $right.on("click", scrollRight);
 });
